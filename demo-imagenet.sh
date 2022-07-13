b4a4f1bc551f6cd3fead62a19e8f68a6f84e9aa6
Yiyou Sun <sunyiyou@cs.wisc.edu>
2022-07-13 12:33:23 -0500

    demo change

--- demo-imagenet.sh
+++ demo-imagenet.sh
@@ -3,7 +3,12 @@ echo ID: ImageNet
 echo Energy
 python ood_eval.py --name imagenet --in-dataset imagenet --model-arch resnet50
 
-echo DICE + ReACT
-python ood_eval.py --name imagenet --in-dataset imagenet --model-arch resnet50 --p 15 --clip_threshold 1.5
+
+echo ID: ImageNet
+echo DICE
+python ood_eval.py --name imagenet --in-dataset imagenet --model-arch resnet50 --p 70
+
+#echo DICE + ReACT
+#python ood_eval.py --name imagenet --in-dataset imagenet --model-arch resnet50 --p 15 --clip_threshold 1.5
 
 
